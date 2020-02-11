fe3b6a02e3bcbe03e27226c9cc961afc360c56a5
Marcin Kaminski <marcin.kaminski@dreamlab.pl>
2020-02-11 10:06:55 +0100

    Updates

---
+++ precache-manifest.316c1ee99f8cb66cb29cf446147085dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a25414dcb0ec3bc35bca5f737598591",
+    "url": "/issue-tracker/index.html"
+  },
+  {
+    "revision": "e2c7d48a5f9abccccfae",
+    "url": "/issue-tracker/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e2c7d48a5f9abccccfae",
+    "url": "/issue-tracker/static/js/2.4e2e5f92.chunk.js"
+  },
+  {
+    "revision": "717ee061c1fb76f42e87",
+    "url": "/issue-tracker/static/js/main.ffa7dd5a.chunk.js"
+  },
+  {
+    "revision": "44fd3bb3001ef150f552",
+    "url": "/issue-tracker/static/js/runtime-main.ced9a44d.js"
+  }
+]);
